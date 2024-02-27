ac3d2d656b048b1764d2decfffcd6144ed6a7c9f
assembouchekifa <147662333+assembouchekifa@users.noreply.github.com>
2024-02-27 17:44:26 +0100

    Nav bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import classNames from "classnames";
+import Nav from "@/components/Nav";
 
 export default function Home() {
   return (
-    <main className="text-danger">
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar w/ text
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarText"
-            aria-controls="navbarText"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarText">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Features
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Pricing
-                </a>
-              </li>
-            </ul>
-            <span className="navbar-text">
-              Navbar text with an inline element
-            </span>
-          </div>
-        </div>
-      </nav>{" "}
-      wzz
-      <Image src={"/logo.svg"} alt={"logo"} width={600} height={600} />
+    <main className="px-4  ">
+      <Nav />
     </main>
   );
 }
---
+++ src/components/Nav.tsx
@@ -0,0 +1,84 @@
+import Image from "next/image";
+import styles from "./Nav.module.css";
+import classNames from "classnames";
+
+function Nav() {
+  return (
+    <nav
+      className={classNames(
+        "navbar px-md-5 px-1 fixed-top min-vw-100 top navbar-expand-lg ",
+        styles.nav
+      )}
+    >
+      <div className="container-fluid  ">
+        <a className="navbar-brand   " href="#">
+          <Image
+            width={100}
+            height={50}
+            src={"/logo.svg"}
+            alt={"logo"}
+            className="text-danger"
+          />
+        </a>
+        <button
+          className="navbar-toggler shadow-none"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse flex-grow-0 navbar-collapse" id="navbarText">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a
+                className={classNames("nav-link  px-3 ", styles.textpage)}
+                aria-current="page"
+                href="#"
+              >
+                Home
+              </a>
+            </li>
+            <li className="nav-item  ">
+              <a
+                className={classNames("nav-link  px-3 ", styles.textpage)}
+                href="#aboutMe"
+              >
+                About Me
+              </a>
+            </li>
+            <li className="nav-item  ">
+              <a
+                className={classNames("nav-link  px-3 ", styles.textpage)}
+                href="#services"
+              >
+                Services
+              </a>
+            </li>
+            <li className="nav-item  ">
+              <a
+                className={classNames("nav-link  px-3 ", styles.textpage)}
+                href="#projects"
+              >
+                Projects
+              </a>
+            </li>
+            <li className="nav-item  ">
+              <a
+                className={classNames("nav-link  px-3 ", styles.textpage)}
+                href="#work"
+              >
+                Work
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
