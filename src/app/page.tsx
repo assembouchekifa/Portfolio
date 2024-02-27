488fce120f05d0f615c54dcb20b623a9f9647dbb
assembouchekifa <147662333+assembouchekifa@users.noreply.github.com>
2024-02-28 00:59:54 +0100

    first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import classNames from "classnames";
-import Nav from "@/components/Nav";
+import Nav from "@/components/Nav/Nav";
+import StrartPage from "@/components/StartPage/StartPage";
 
 export default function Home() {
   return (
     <main className="px-4  ">
       <Nav />
+      <div style={{ height: "76px" }}></div>
+      <StrartPage />
     </main>
   );
 }
---
+++ src/components/StartPage/StartPage.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+import styles from "./StartPage.module.css";
+import classNames from "classnames";
+
+function StartPage() {
+  return (
+    <main className=" mt-5 ">
+      <div className="container text-center">
+        <div className="row">
+          <div
+            className={classNames(
+              "col-12 col-lg-6 d-flex flex-column align-items-center my-5 "
+            )}
+          >
+            {" "}
+            <div className="d-flex flex-column align-items-start">
+              <p className={classNames("mb-0", styles.textpage)}>Hello</p>
+              <p className={classNames("fs-1 fw-bold", styles.textpage)}>
+                I&apos;m Assem
+              </p>
+              <p
+                className={classNames(
+                  "text-start fs-4 fw-bold opacity-75 ",
+                  styles.textpage
+                )}
+              >
+                Web Developer <br /> frontend & backend
+              </p>{" "}
+              <button
+                className={classNames("btn px-5  py-2 fw-bold ", styles.but)}
+              >
+                HIRE ME
+              </button>
+            </div>{" "}
+          </div>
+          <div
+            className={classNames(
+              "col-12 col-lg-6  position-relative ",
+              styles.photo
+            )}
+          >
+            <Image
+              src={"/bgphoto.svg"}
+              className="object-fit-contain"
+              alt={"photobg"}
+              fill
+            />
+            <Image
+              src={"/mypoto.png"}
+              className="object-fit-contain"
+              alt={"May photo"}
+              fill
+            />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default StartPage;
